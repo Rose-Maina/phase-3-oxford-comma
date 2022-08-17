cf95391ac9aa9556b13c1c0d9664b8b4c77fee26
Rose Maina <rosewairimu27@gmail.com>
2022-08-17 15:11:38 +0300

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
+    return array.join(" and ") if array.length < 3
 
+  # inserting 'and ' before the last string in array
+  array[-1] = "and #{array[-1]}"
+
+  array.join(", ") 
 end
