961dcf47c9c458f5c6fe8d9c51948092588f327c
Rose Maina <rosewairimu27@gmail.com>
2022-08-17 15:12:58 +0300

    commit tree f9c1258c1be591dc4162ecb480c4e5d8789db9f9

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,10 @@
 def oxford_comma(array)
+    #
     return array.join(" and ") if array.length < 3
 
   # inserting 'and ' before the last string in array
   array[-1] = "and #{array[-1]}"
 
   array.join(", ") 
+
 end
